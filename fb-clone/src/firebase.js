c4cf64e655d6d1502273ced118e7f01e107e28eb
Rahul <rahulv0530@gmail.com>
2021-01-05 01:12:57 +0530

    Basic

---
+++ fb-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAI_y23YG_7oXOMF3q_J_3Uxt51QzFmQ2Q",
+    authDomain: "fb-clone-e49dc.firebaseapp.com",
+    projectId: "fb-clone-e49dc",
+    storageBucket: "fb-clone-e49dc.appspot.com",
+    messagingSenderId: "235469851711",
+    appId: "1:235469851711:web:1cb39c0ee1c9d81921fc8a",
+    measurementId: "G-T7LJ19E1ZC"
+};
