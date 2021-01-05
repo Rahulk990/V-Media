7a1661dad6148192f9e8a0ca390a32094e694af5
Rahul <rahulv0530@gmail.com>
2021-01-05 22:59:24 +0530

    Flex Change

--- client/src/Components/Feed.js
+++ client/src/Components/Feed.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import './Feed.css'
 
+import PostSender from './PostSender'
+// import Post from './Post'
+
 const Feed = () => {
     return (
         <div className='feed'>
             
+            {/* Create Post */}
+            <PostSender />    
+            
+            {/* Posts */}
+            {/* {
+                postData.map(entry => (
+                    <Post 
+                        profilePic={entry.avatar}
+                        text={entry.text}
+                        timestamp={entry.timestamp}
+                        imgName={entry.imgName}
+                        username={entry.user}
+                    />
+                ))
+            } */}
+        
+        
         </div>
     )
 }
