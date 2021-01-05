cdff8bfb69c7703d04d3f1687759270db34abf46
Rahul <rahulv0530@gmail.com>
2021-01-05 14:22:36 +0530

    Masking

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,13 @@ function App() {
   return (
     <div className="app">
       <h1> Hello World</h1>
+      {/* Navbar */}
+      {/* Sidebar */}
+      {/* Feed */}
+      {/* Widgets */}
+        {/* To-do List */}
+        {/* Schedule */}
+
     </div>
   );
 }
