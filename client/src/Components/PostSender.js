97bd8119de1bd193b2b83fa99253cda69ffd3928
Rahul <rahulv0530@gmail.com>
2021-01-05 23:35:58 +0530

    Add Post

--- client/src/Components/PostSender.js
+++ client/src/Components/PostSender.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import './PostSender.css'
+
+import { Avatar } from '@material-ui/core'
 
 const PostSender = () => {
     return (
-        <div>
-            
+        <div classname='postSender'>
+            <div className='postSender__top'>
+                <Avatar />
+
+                <form>
+                    <input type='text' placeholder="What's on your mind."/>
+                    <input type='text' placeholder='Post Button'/>
+                </form>
+
+            </div>
+
+            <div className='postSender__bottom'>
+
+            </div>
         </div>
     )
 }
