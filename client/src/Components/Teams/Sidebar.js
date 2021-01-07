5c67628b76751314184e05cafd341fdcac4d4a90
Rahul <rahulv0530@gmail.com>
2021-01-07 13:29:30 +0530

    Add Create New Team Option

--- client/src/Components/Teams/Sidebar.js
+++ client/src/Components/Teams/Sidebar.js
@@ -1,21 +1,47 @@
+import { Button } from '@material-ui/core'
 import React from 'react'
 import './Sidebar.css'
 
 import SidebarRow from './SidebarRow'
 
 const Sidebar = () => {
+
+    const isTeam = false;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Open Dialog Box
+    }
+
     return (
         <div className='sidebar'>
+
+            <div className='sidebar__new'>
+                <Button onClick={handleSubmit}>
+                    Create New Team
+                </Button>
+            </div>
+
             <div className='sidebar__heading'>
                 <p> Your Teams </p>
             </div>
 
-            <SidebarRow src="S" title='Team1' />
-            <SidebarRow src="S" title='Team1' />
-            <SidebarRow src="S" title='Team1' />
-            <SidebarRow src="S" title='Team1' />
-            <SidebarRow src="S" title='Team1' />
-            <SidebarRow src="S" title='Team1' />
+            {(isTeam) ? (
+                <div className='sidebar__rows'>
+                    <SidebarRow src="S" title='Team1' />
+                    <SidebarRow src="S" title='Team1' />
+                    <SidebarRow src="S" title='Team1' />
+                    <SidebarRow src="S" title='Team1' />
+                    <SidebarRow src="S" title='Team1' />
+                    <SidebarRow src="S" title='Team1' />
+                </div>
+
+            ) : (
+                    <div className='sidebar__error'>
+                        <p> Nothing to Display </p>
+                    </div>
+                )}
+
         </div >
     )
 }
