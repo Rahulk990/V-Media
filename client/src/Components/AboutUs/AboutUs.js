d2d1b36c1883e72026f1211a3c2ea541eb0cdb8e
Puneet <puneetjangid349@gmail.com>
2021-01-21 16:34:32 +0530

    about us links

--- client/src/Components/AboutUs/AboutUs.js
+++ client/src/Components/AboutUs/AboutUs.js
@@ -1,84 +1,100 @@
-import React from 'react'
-import './AboutUs.css'
+import React from "react";
+import "./AboutUs.css";
 import {
 	LinkedIn,
 	GitHub,
 	PermIdentityRounded,
-    Twitter,
-    Mail,
+	Twitter,
+	Mail,
 } from "@material-ui/icons";
 import { IconButton, Tooltip } from "@material-ui/core";
 const AboutUs = () => {
-    return (
-        <div className='about'>
-            <div className='about__heading'>
-                Meet the Team
-            </div>
-            <div className='card_container'>
-            <div className='card_1 card'>
-                <div className='card_img'>
-                    <img src = 'https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg'/>
-                </div>
-                
-                <div className='card_socialmedia'>
-                    <IconButton className='blue'>
-                        <LinkedIn/>
-                    </IconButton>
-                    <IconButton className='black'>
-                        <GitHub/>
-                    </IconButton>
-                    <IconButton className='blue'>
-                        <Twitter/>
-                    </IconButton>
-                    <IconButton className='red'>
-                        <Mail/>
-                    </IconButton>
-                </div>
-                <div className='name align_center'>
-                    <h3>Puneet Jangid</h3>
-                </div>
-                <div className='college_info'>
-                    <div className='college_name align_center'>
-                        <h5>Delhi Technological University</h5>
-                    </div>
-                    <div className='degree align_center'>
-                        <h6>Computer Engineering</h6>
-                    </div>
-                </div>
-            </div>
-            <div className='card_2 card'>
-                <div className='card_img'>
-                    <img src = 'https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg'/>
-                </div>
-                <div className='card_socialmedia'>
-                    <IconButton className='blue'>
-                        <LinkedIn/>
-                    </IconButton>
-                    <IconButton className='black'>
-                        <GitHub/>
-                    </IconButton>
-                    <IconButton className='blue'>
-                        <Twitter/>
-                    </IconButton>
-                    <IconButton className='red'>
-                        <Mail/>
-                    </IconButton>
-                </div>
-                <div className='name align_center'>
-                    <h3>Rahul</h3>
-                </div>
-                <div className='college_info'>
-                    <div className='college_name align_center'>
-                        <h5>Delhi Technological University</h5>
-                    </div>
-                    <div className='degree align_center'>
-                        <h6>Computer Engineering</h6>
-                    </div>
-                </div>
-            </div>
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className="about">
+			<div className="about__heading">Meet the Team</div>
+			<div className="card_container">
+				<div className="card_1 card">
+					<div className="card_img">
+						<img src="https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg" />
+					</div>
+                    <div className="card_socialmedia">
+						<form action='https://www.linkedin.com/in/puneet-jangid-26a0ba16b/' target="_blank">
+							<IconButton className="blue" type='submit'>
+								<LinkedIn />
+							</IconButton>
+						</form>
+                        <form action='https://github.com/Napster404' target="_blank">
+						<IconButton className="black" type='submit'>
+							<GitHub />
+						</IconButton>
+                        </form>
+                        <form action='https://twitter.com/Puneetjangid1' target="_blank">
+						<IconButton className="blue" type='submit'>
+							<Twitter />
+						</IconButton>
+                        </form>
+                        <form action='mailto:puneetjangid349@gmail.com' target="_blank">
+						<IconButton className="red" type='submit'>
+							<Mail />
+						</IconButton>
+                        </form>
+					</div>
+					
+					<div className="name align_center">
+						<h3>Puneet Jangid</h3>
+					</div>
+					<div className="college_info">
+						<div className="college_name align_center">
+							<h5>Delhi Technological University</h5>
+						</div>
+						<div className="degree align_center">
+							<h6>Computer Engineering</h6>
+						</div>
+					</div>
+				</div>
+				<div className="card_2 card">
+					<div className="card_img">
+						<img src="https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg" />
+					</div>
+					<div className="card_socialmedia">
+                    <div className="card_socialmedia">
+						<form action='' target="_blank">
+							<IconButton className="blue" type='submit'>
+								<LinkedIn />
+							</IconButton>
+						</form>
+                        <form action='' target="_blank">
+						<IconButton className="black" type='submit'>
+							<GitHub />
+						</IconButton>
+                        </form>
+                        <form action='' target="_blank">
+						<IconButton className="blue" type='submit'>
+							<Twitter />
+						</IconButton>
+                        </form>
+                        <form action='' target="_blank">
+						<IconButton className="red" type='submit'>
+							<Mail />
+						</IconButton>
+                        </form>
+					</div>
+					</div>
+					<div className="name align_center">
+						<h3>Rahul</h3>
+					</div>
+					<div className="college_info">
+						<div className="college_name align_center">
+							<h5>Delhi Technological University</h5>
+						</div>
+						<div className="degree align_center">
+							<h6>Computer Engineering</h6>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default AboutUs
+export default AboutUs;
